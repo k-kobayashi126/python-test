38f4d337afb79037862c43dca10a537a0e28b927
k-kobayashi <k.kounosuke126@gmail.com>
2026-02-05 20:39:25 +0900

    2026/02/05 Pythonをmain関数などを使って作成

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    プログラムのメイン処理を行う関数
+    """
+    message = "Hello Docker Python!"
+    print(message)
+
+# このファイルが「直接実行された時」だけ、main()を呼び出す
+if __name__ == "__main__":
+    main()
